42a96457dcf55f7884ccb3aad56dcd9fdc996dab
audith <audithadhikary081@gmail.com>
2026-04-17 17:14:06 +0600

    dependency add

--- app/dependencies.py
+++ app/dependencies.py
@@ -19,9 +19,13 @@ def get_current_user(token:str,db:Session=Depends(get_db)):
     try:
         playload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
         user=db.query(User).filter(User.email == playload["sub"]).first()
+        return user
     except:
         raise HTTPException(status_code=401,detail="invalid user name")
     
 
-
+def admin_only(user:User=Depends(get_current_user)):
+    if not user.is_admin:
+        raise HTTPException(status_code=403,detail="Admin only")
+    return user
 
