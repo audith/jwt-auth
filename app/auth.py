1cac366fb1554c4fc2f4dd20a314bcf3f6a85cdd
audith <audithadhikary081@gmail.com>
2026-04-17 14:31:05 +0600

    jwt token create for 60 minit

---
+++ app/auth.py
@@ -0,0 +1,29 @@
+from jose import jwt 
+from datetime import datetime,timedelta
+from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM=os.getenv("ALGORITHM")
+
+pwd_context=CryptContext(schemes=["bcrypt"])
+
+def hash_password(password:str):
+    return pwd_context.hash(password)
+
+def verify_password(plain,hashed):
+    return pwd_context.verify(plain,hashed)
+
+
+def create_token(data:dict):
+    to_encode=data.copy()
+    expire=datetime.utcnow()+timedelta(minutes=60)
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+
+
+
